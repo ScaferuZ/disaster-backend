182b8ff5ec42401cc206a7fce0f722f7aa9bfc03
scaf <endrafruz@gmail.com>
2026-02-18 22:13:22 +0700

    feat: adding pwa functionalities

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -11,6 +11,13 @@ const noStore = {
 route.get("/", serveStatic({ path: "./public/index.html", ...noStore }));
 route.get("/app.js", serveStatic({ path: "./public/app.js", ...noStore }));
 route.get("/sw.js", serveStatic({ path: "./public/sw.js", ...noStore }));
+route.get("/manifest.webmanifest", serveStatic({ path: "./public/manifest.webmanifest", ...noStore }));
+route.get("/favicon.svg", serveStatic({ path: "./public/favicon.svg", ...noStore }));
+route.get("/icons/icon.svg", serveStatic({ path: "./public/icons/icon.svg", ...noStore }));
+route.get(
+	"/icons/icon-maskable.svg",
+	serveStatic({ path: "./public/icons/icon-maskable.svg", ...noStore }),
+);
 route.get("/receiver", serveStatic({ path: "./public/receiver.html", ...noStore }));
 route.get("/receiver.js", serveStatic({ path: "./public/receiver.js", ...noStore }));
 
